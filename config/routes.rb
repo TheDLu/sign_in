ba05d0e1ff7efba18836a882a07fecddcfb351ff
Daryl Lu <the.daryl.lu@gmail.com>
2014-04-25 16:31:33 -0400

    enables residents to be editable only by the correct user (ower), as well as added the ability for a non-logged-in person select the Show action for a visit. Next step is to enable the person to create a new visit and enter Guest Information.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ SignIn::Application.routes.draw do
   root "pages#home"
   get "residences/home"
   get "residents/home"
+  get "visits/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
